29a001d273193e8fa3d44abe5fc9685031b78920
nagmengarcia <nagmengarcia@gmail.com>
2024-03-09 14:46:29 -0300

    funciona agregar por categoria, voy a intentar quitar solo un elemento en el carrito con contador y botnes -+

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var productos = [
+    {
+        id: 1,
+        nombre: "Champi King de carne",
+        imagen: "URL_DE_LA_IMAGEN",
+        descripcion: "DESCRIPCION_DEL_PRODUCTO",
+        precio: 8500,
+        categoria: "hamburguesas",
+        cantidad: 0
+    },
+    {
+        id: 2,
+        nombre: "Hamburguesa de queso tristongo",
+        imagen: "URL_DE_LA_IMAGEN",
+        descripcion: "DESCRIPCION_DEL_PRODUCTO",
+        precio: 5000,
+        categoria: "hamburguesas",
+        cantidad: 0
+    },
+    // Otros productos...
+];
+
