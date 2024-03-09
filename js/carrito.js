5890ddd9fd858a5874bba77e6988de2fe780a5c7
nagmengarcia <nagmengarcia@gmail.com>
2024-03-09 17:21:12 -0300

    complete la estructura html y css de la botonera, falta buscarle la vuelta a la funcionalidad

--- js/carrito.js
+++ js/carrito.js
@@ -6,8 +6,14 @@ function renderFinalCart() {
         for (const producto of cart) {
             a += `<div class="cart-bill-list">
                 <img src="${producto.image}" alt="${producto.productName}" width="72"/>
-                <p class="p-name">${producto.productName} <b> x ${producto.cantidad} </b></p> 
-                <p class="p-price">$${producto.price}</p> 
+                <div class="productNameAndCounter">
+                <p class="p-name">${producto.productName}</p> <div class="itemCounter"> <button> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
+                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
+              </svg></button> <p>${producto.cantidad}</p> <button> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+            </svg></button></div>
+            </div>
+                <p class="p-price">$${producto.price * producto.cantidad}</p> 
                 <button class="removeFromCart" id="${producto.id}" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                 <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
                 </svg></button>
